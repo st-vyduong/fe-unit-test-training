e6280f4d767e98920ae92d7b8e5f3e461b8ef889
st-vyduong <vy.duong@supremetech.vn>
2023-04-18 09:52:10 +0700

    exercise 4

---
+++ src/app/core/class/order.ts
@@ -0,0 +1,60 @@
+export type Discount = {
+  percent: number;
+  conditionQuantity: number;
+}
+
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+  discount: Discount[];
+}
+
+export type LineItem = {
+  product: Product;
+  quantity: number
+}
+
+interface IOrder {
+  lineItemList: LineItem[];
+}
+
+export class Order implements IOrder {
+  lineItemList: LineItem[] = [];
+
+  constructor(data: LineItem[]) {
+    this.lineItemList = [...data];
+  }
+
+  getOrder() {
+    return this.lineItemList;
+  }
+
+  getLineItem(id: number) {
+    return this.lineItemList.find((item: LineItem) => item.product.id === id);
+  }
+
+  addLineItem(lineItem: LineItem) {
+    this.lineItemList.push(lineItem);
+  }
+
+  removeLineItem(id: number) {
+    this.lineItemList = this.lineItemList.filter((item: LineItem) => item.product.id !== id );
+  }
+
+  removeOrder() {
+    this.lineItemList = [];
+  }
+
+  getTotalPayment() {
+    return this.lineItemList.reduce((sum, lineItem: LineItem) => {
+      let discountMax = 0;
+      lineItem.product.discount.forEach((discount: Discount) => {
+        if (lineItem.quantity >= discount.conditionQuantity && discount.percent > discountMax) {
+          discountMax = discount.percent;
+        }
+      });
+      return sum + lineItem.product.price * lineItem.quantity * (100 - discountMax) / 100;
+    }, 0);
+  }
+}
